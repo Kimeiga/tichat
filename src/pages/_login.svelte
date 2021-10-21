408670b3d478c48d294bcd2ae6335a19f353caa0
Hakan Alpay <hak7alp@gmail.com>
2021-10-21 12:36:36 -0700

    firebase ui bug fix

--- src/pages/_login.svelte
+++ src/pages/_login.svelte
@@ -7,49 +7,54 @@
   const db = firebase.firestore();
 
   onMount(() => {
-    firebase
-      .auth()
-      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-      .then(() => {
-        let ui =
-          firebaseui.auth.AuthUI.getInstance() ||
-          new firebaseui.auth.AuthUI(firebase.auth());
-        let uiConfig = {
-          callbacks: {
-            signInSuccessWithAuthResult: (authResult) => {
-              // this is a new user, add them to the firestore users collection!
+    // only run this if the firebase UI is not already initialized
+    if (!document.querySelector(".firebaseui-card-content")) {
+      firebase
+        .auth()
+        .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(() => {
+          let ui =
+            firebaseui.auth.AuthUI.getInstance() ||
+            new firebaseui.auth.AuthUI(firebase.auth());
+          let uiConfig = {
+            callbacks: {
+              signInSuccessWithAuthResult: (authResult) => {
+                // this is a new user, add them to the firestore users collection!
 
-              // if (authResult.additionalUserInfo.isNewUser) {
-              db.collection("users")
-                .doc(authResult.user.uid)
-                .set({
-                  displayName: authResult.user.displayName,
-                  photoURL: authResult.user.photoURL,
-                  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-                })
-                .then(() => {
-                  console.log("User document successfully written!");
-                })
-                .catch((error) => {
-                  console.error("Error writing user document: ", error);
-                });
-              // }
-              return false;
+                // if (authResult.additionalUserInfo.isNewUser) {
+                db.collection("users")
+                  .doc(authResult.user.uid)
+                  .set({
+                    displayName: authResult.user.displayName,
+                    photoURL: authResult.user.photoURL,
+                    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                  })
+                  .then(() => {
+                    console.log("User document successfully written!");
+                  })
+                  .catch((error) => {
+                    console.error("Error writing user document: ", error);
+                  });
+                // }
+                return false;
+              },
             },
-          },
-          signInOptions: [
-            {
-              provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-              clientId:
-                "561811199163-mlumhdkfre3068f2o981lljj5srh9f0h.apps.googleusercontent.com",
-            },
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          ],
-          // Required to enable one-tap sign-up credential helper.
-          credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-        };
-        ui.start("#firebaseui-auth-container", uiConfig);
-      });
+            signInOptions: [
+              {
+                provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                clientId:
+                  "561811199163-mlumhdkfre3068f2o981lljj5srh9f0h.apps.googleusercontent.com",
+              },
+              firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            ],
+            // Required to enable one-tap sign-up credential helper.
+            credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+          };
+
+          // run this only if the container isn't already initialized lol
+          ui.start("#firebaseui-auth-container", uiConfig);
+        });
+    }
   });
 </script>
 
